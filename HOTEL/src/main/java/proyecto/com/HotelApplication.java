284a3003241a715946bb940b4386ff457d7cfb53
DavidSanabriaVindas <dsanabria30061@ufide.ac.cr>
2025-11-18 00:55:46 -0600

    Conexion v2

--- HOTEL/src/main/java/proyecto/com/HotelApplication.java
+++ HOTEL/src/main/java/proyecto/com/HotelApplication.java
@@ -10,14 +10,7 @@ public class HotelApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelApplication.class, args);
 
-        Conexion conexion = new Conexion();
-        Connection conn = conexion.conectar();
+       
+}
 
-        if (conn != null) {
-            System.out.println("La conexión funciona correctamente desde Java.");
-          
-        } else {
-            System.out.println("Error en la conexión.");
-        }
-    }
 }
---
+++ HOTEL/src/main/java/proyecto/com/controller/testController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RestController  
+public class testController {
+    
+     @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/probarConexion")
+    public String probarConexion() {
+        try (Connection conn = dataSource.getConnection()) {
+            return "✔ Conexión a Oracle EXITOSA!";
+        } catch (Exception e) {
+            return "❌ ERROR de conexión: " + e.getMessage();
+        }
+    }
+}
