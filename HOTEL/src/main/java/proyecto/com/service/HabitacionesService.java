0478b23dde5b61d3f3598839523b6db30a2a67ca
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2025-12-10 22:21:55 -0600

    ssss
    ssss

--- HOTEL/src/main/java/proyecto/com/service/HabitacionesService.java
+++ HOTEL/src/main/java/proyecto/com/service/HabitacionesService.java
@@ -1,39 +1,14 @@
 
 package proyecto.com.service;
+
 import proyecto.com.model.Habitaciones;
 import proyecto.com.repository.HabitacionesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class HabitacionesService {
-
-    public Object listar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public Object obtenerPorId(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String guardarHabitacion(Habitaciones habitacion) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String actualizarHabitacion(Habitaciones habitacion) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String eliminar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public Habitaciones buscarPorId(int idHabitacion) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
-    @Service
-public class HabitacionService {
 
     @Autowired
     private HabitacionesRepository repo;
@@ -61,7 +36,4 @@ public class HabitacionService {
     public Habitaciones buscarPorId(int id) {
         return repo.buscarPorIdConFuncion(id);
     }
-}
-
-    
 }
